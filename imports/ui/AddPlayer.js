d20b04ecdaf9c306a53134366c84cd56b04665a0
rmartinez1 <ronaldbmartinez95@gmail.com>
2019-01-24 00:31:07 -0500

    'first'

---
+++ imports/ui/AddPlayer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {Players} from './../api/players';
+
+export default class AddPlayer extends React.Component {
+    handleSubmit(e) {
+            let playerName = e.target.playerName.value;
+
+            e.preventDefault();
+
+            //validates the string in the subtmit form
+            if (playerName) {
+              e.target.playerName.value='';
+            //Player Insert
+            Players.insert({
+            name: playerName,
+            score: 0
+            });
+        }     
+    }
+    render() {
+        return (
+            <div className='item'>
+                <form className='form' onSubmit={this.handleSubmit.bind(this)}>
+                <input className='form__input' type="text" name="playerName" placeholder="Player Name"/>
+                    <button className='button'>Add Player</button>
+            </form>
+            </div>
+        );
+    }
+};
